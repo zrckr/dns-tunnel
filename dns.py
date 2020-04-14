39016b9eb7c7ddf1ee767b7ead2cb0b90efc9a19
Zerocker <zerocloude@gmail.com>
2020-04-14 19:17:56 +0900

    Scrambling and descrambling bit sequences

---
+++ dns.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import bitstring as bitstr
+
+def scramble(data, offset, reverse=False):
+    a = bitstr.BitArray(data, endian="little")
+    b = bitstr.BitArray(length=len(a))
+    
+    if len(offset) != 2:
+        raise ValueError("The offset must be a tuple with two values!")
+    p1, p2 = offset
+
+    for i in range(len(a)): 
+        if (i < p1):
+            b[i] = a[i]
+        elif (i < p2 ):
+            x = (a[i-p1] if reverse else b[i-p1])
+            b[i] = a[i] ^ x
+        else:
+            x = (a[i-p1] if reverse else b[i-p1])
+            y = (a[i-p2] if reverse else b[i-p2 ])
+            b[i] = a[i] ^ x ^ y
+
+    return b.tobytes()
