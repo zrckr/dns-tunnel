f4d1c40331640567c786c31918bd98980ad25f7c
Zerocker <zerocloude@gmail.com>
2020-04-23 21:39:46 +0900

    Added text messaging mode. Added file uploading mode. Added random bytes generation mode. Now client generates DNS queries.

--- client.py
+++ client.py
@@ -2,20 +2,64 @@
 # coding: utf-8
 
 import sys
-import dnslib
+import time
+import base64
+import random
 import socket
-import timeit
+import struct
+import hashlib
 import binascii
 import argparse
-import constants as cn
+
+import dnslib as dns
 import exfiltration as exf
 
 class Client():
-    def __init__(self, host, port, timeout=30):
+    def __init__(self, addr, args):
         self.sock = None
-        self.addr = (host, port)
-        self.timeout = timeout
+        self.addr = addr
+        self.timeout = args.timeout
+        self.domain = args.domain
+        self.is_text = args.text
+        self.is_file = args.file
+        self.is_rand = args.rand
+        self.qtype = args.qtype
     
+    def run(self):
+        try:
+            while True:
+                data = None
+                if (self.is_text):
+                    data = self.read_text()
+                elif (self.is_rand):
+                    data = self.read_random()
+                    time.sleep(5)
+                elif (self.is_file):
+                    data = self.read_file()
+                hash_ = hashlib.sha1(data).hexdigest()
+
+                new_data = b''
+                for q in self.dns_ask(data):
+                    response = None
+                    if (len(q) > exf.MAX_MSG_LEN):
+                        response = self.send(q, socket.SOCK_STREAM)
+                    else:
+                        response = self.send(q, socket.SOCK_DGRAM)
+                    new_data += self.dns_fin(response)
+                
+                hash__ = hashlib.sha1(new_data).hexdigest()
+                print("$", hash_, hash__ == hash_)
+
+                if (self.is_file):
+                    break
+                
+        except KeyboardInterrupt:
+            print("[Interrupt] Exit by the user...")
+        except Exception as error:
+            print("[Info]", str(error))
+        except socket.error as error:
+            print("[Socket]", str(error))
+
     def send(self, data, protocol):
         with socket.socket(socket.AF_INET, protocol) as sock:
             if protocol is socket.SOCK_STREAM:
@@ -25,53 +69,73 @@ class Client():
             sock.connect(self.addr)
             sock.send(data)
 
-            response = sock.recv(cn.SOCK_BUFFER_SIZE)
+            response = sock.recv(exf.SOCK_BUFFER_SIZE)
             if not response:
                 raise Exception('Sending a request successfully failed!')
             return response
 
-    def run(self):
-        try:
-            while True:    
-                text = str.encode(input("> "))
-                data = exf.dns_to_q(text, 'google.com', 'A')
+    def read_text(self):
+        text = input("> ").encode()
+        header = struct.pack("!4sI", b'text', len(text))
 
-                response = None
-                if (len(data) > cn.MAX_MSG_LEN):
-                    response = self.send(data, socket.SOCK_STREAM)
-                else:
-                    response = self.send(data, socket.SOCK_DGRAM)
-                
-                response = exf.dns_from_q(response)
+        return header + text
 
-                print("$", response.decode())
-                
-        except KeyboardInterrupt:
-            print("[Interrupt] Exit by the user...")
-        except Exception as error:
-            print("[Info]", str(error))
-        except socket.error as error:
-            print("[Socket]", str(error))
+    def read_random(self):
+        size = random.choice(
+            [8, 16, 32, 64, 128, 256, 512, 1024])
+        
+        rand = exf.random_bytes(size)
+        header = struct.pack("!4sH", b'rand', len(rand))
+
+        return header + rand
+
+    def read_file(self, buffer=32):
+        whole = b''
+        with open(self.is_file, 'rb') as file:
+            data = file.read(buffer)
+            while (data):
+                whole += data
+                data = file.read(buffer)
+        
+        header = struct.pack("!4sH", b'file', len(whole))
+        
+        return header + whole
+
+    def dns_ask(self, data):
+        labels = exf.domain_encode(data, self.domain, base64.urlsafe_b64encode)
+        queries = [dns.DNSRecord.question(label, self.qtype).pack() for label in labels]
+        return queries
+
+    def dns_fin(self, answer):
+        reply = dns.DNSRecord.parse(answer)
+        return reply.rr[0].rdata.data[0]
 
 # --------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(
-        description="Client script"
-    )
+    parser = argparse.ArgumentParser(description="DNS Client script")
+    
     parser.add_argument('-c', '--connect', dest='conn', type=str, required=True,
                         help='Establishes a connection to the server at the specified address:port')
     
+    parser.add_argument('-t', '--timeout', dest='timeout', type=int, default=30,
+                        help="Specifies the timeout for server response")
+    
     parser.add_argument('-st', '--send-text', dest='text', action='store_true',
                         help='Sends a text string to the server')
+    
     parser.add_argument('-sf', '--send-file', dest='file', type=str,
                         help='Sends the file to the server. The file path is required.')
+    
     parser.add_argument('-sr', '--send-random', dest='rand', action='store_true',
                         help='Sends a random byte array to the server')
-
-    parser.add_argument('-rr', '--rdata-type', dest='rdata', type=str,
-                        help='Specifies the type of record to attach data to for transmission')
+    
+    parser.add_argument('-d', '--domain', dest='domain', type=str, default='example.com',
+                        help='Specifies the domain name')
+    
+    parser.add_argument('-q', '--qtype', dest='qtype', type=str, default="A",
+                        help='Specifies the type of record for a DNS question')
 
     args = parser.parse_args()
     
@@ -82,6 +146,13 @@ if __name__ == "__main__":
         (args.file and args.rand):
        parser.error('Only or at least one sending mode must be specified!')
 
-    dest = args.conn.split(':')
-    client = Client(dest[0], int(dest[1]))
+    if (args.text):
+        print("[Mode]", f"Sending text messages ...")
+    elif (args.file):
+        print("[Mode]", f"Sending {args.file} ...")
+    elif (args.rand):
+        print("[Mode]", f"Sending random bytes ...")
+
+    addr = args.conn.split(':')
+    client = Client((addr[0], int(addr[1])), args)
     client.run()
