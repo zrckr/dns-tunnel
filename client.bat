8df3fd8e6b0cc992aa4a4150ba0eca53eca50ca6
Zerocker <zerocloude@gmail.com>
2020-04-26 17:26:28 +0900

    Created .bat files for running scripts with args.

---
+++ client.bat
@@ -0,0 +1,2 @@
+@echo off
+python.exe client.py --connect "localhost:53" --send-random --timeout 60 --scramble 3 11
---
+++ server.bat
@@ -0,0 +1,2 @@
+@echo off
+python.exe server.py --zone-file zone.cfg --debug --port 53 --timeout 60
