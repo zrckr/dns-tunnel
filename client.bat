fa10d0245a8ac2bf0788422e8c927fe692a3f379
Zerocker <zerocloude@gmail.com>
2020-05-20 17:49:49 +0900

    Minor changes

--- client.bat
+++ client.bat
@@ -1,2 +1,3 @@
 @echo off
-python.exe client.py --connect "localhost:53" --send-random --timeout 10 --qtype "AAAA" --aes "ohyeah"
+:: --aes "ohyeah"
+python.exe client.py --connect "localhost:53" --send-text --timeout 10 --qtype "A"
